a82003e27a960568391d56dd3bb1ef61ad439412
Neeraj <neeraj.singh@60degree.com>
2013-11-13 13:37:20 +0530

    validations on playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,6 @@ class Playlist < ActiveRecord::Base
   belongs_to :host
   has_many :playlist_songs
   has_many :songs, through: :playlist_songs
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :host_id, presence: true, numericality: { only_integer: true }
 end
