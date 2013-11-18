67c2d7b75ee0d54e4b1fa4a0a82dab20fb109c5e
Neeraj <neeraj.singh@60degree.com>
2013-11-18 14:03:32 +0530

    add mail settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Mysong::Application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => 587,
